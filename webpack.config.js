f8665f3200fc51264310a1fa6dc4a69806bef9d0
wefashe <wefashe@qq.com>
2019-01-09 17:07:29 +0800

    配置webpack环境

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,142 @@
 // webpack v4
-const path = require('path');
-const extractTextWebpackPlugin = require('extract-text-webpack-plugin');
+const path = require('path')
+const HtmlPlugin = require('html-webpack-plugin')
+const CleanPlugin = require('clean-webpack-plugin')
+// extract-text-webpack-plugin在webpack 4表现并不好，mini-css-extract-plugin代替
+// 需要把webpack升级到4.2.0.0，不然这个插件无法运行！
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const internalIp = require('internal-ip')
 module.exports = {
-  entry: path.join(__dirname, 'src/main.js'),
+  mode: 'development',
+  devtool: 'source-map',
+  entry: path.resolve(__dirname, 'src/main.js'),
   output: {
+    // js引用路径或者CDN地址
+    publicPath: '/assets/',
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.[hash:8].js',
+    // hash 所有打包文件名hash相同  chunkhash 每个打包文件名hash都不相同
+    filename: '[name].[hash:8].js'
   },
   module: {
     rules: [{
-      //es6就是未来，可是现在的浏览器对他的支持不是很好，我们需要babel来转换他
+      // es6就是未来，可是现在的浏览器对他的支持不是很好，我们需要babel来转换他
       test: /\.js$/,
       exclude: /node_modules/,
       use: {
-        loader: "babel-loader"
+        loader: 'babel-loader'
       }
     }, {
       test: /\.css$/,
-      use: extractTextWebpackPlugin.extract({
-        fallback: 'style-loader',
-        use: ['css-loader']
-      })
-    }]
+      use: [MiniCssExtractPlugin.loader, 'style-loader', 'css-loader', 'postcss-loader']
+    },
+    {
+      test: /\.html$/,
+      use: [{
+        loader: 'html-loader',
+        options: {
+          minimize: true,
+          // html 中的 <img> 标签没法使用这个别名功能，但 html-loader 有一个 root 参数，
+          // 可以使 / 开头的文件相对于 root 目录解析
+          root: path.resolve(__dirname, 'src'),
+          attrs: ['img:src', 'link:href']
+        }
+      }]
+    }
+    ]
+  },
+  plugins: [
+    // 清空dist文件夹
+    new CleanPlugin(['dist']),
+    // 在dist生成index.html页面
+    new HtmlPlugin({
+      // 要用html模版，必须安装html-loader
+      template: path.resolve(__dirname, 'src/index.html'),
+      favicon: path.resolve(__dirname, 'src/assets/image/favicon.ico'),
+      filename: path.resolve(__dirname, 'dist/index.html'),
+      // 内置html-minifier压缩插件
+      minify: {
+        // 默认值都为false
+        // 清理注释
+        removeComments: true,
+        // 清理html中的空格、换行符 但span元素内字符串包含的空格没有被清理
+        collapseWhitespace: true,
+        // 压缩html内的样式
+        minifyCSS: true,
+        // 压缩html内的js。
+        minifyJS: true
+      }
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css'
+    })
+  ],
+  optimization: {
+    // 提取公共代码
+    splitChunks: {
+      cacheGroups: {
+        // 注意: priority属性
+        // 其次: 打包业务中公共代码
+        common: {
+          name: 'common',
+          chunks: 'all',
+          minSize: 1,
+          priority: 0
+        },
+        // 首先: 打包node_modules中的文件
+        vendor: {
+          name: 'vendor',
+          test: /[\\/]node_modules[\\/]/,
+          chunks: 'all',
+          priority: 10
+          // enforce: true
+        }
+      }
+    }
+  },
+
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist'),
+    host: '0.0.0.0',
+    port: 8000,
+    hot: true,
+    // 开发环境允许其他电脑访问
+    // hot: {
+    //   host: {
+    //     client: internalIp.v4.sync(),
+    //     server: '0.0.0.0'
+    //   }
+    // },
+    overlay: true,
+    dev: {
+      /*
+      指定 webpack-dev-middleware 的 publicpath
+      一般情况下与 output.publicPath 保持一致（除非 output.publicPath 使用的是相对路径）
+      https://github.com/webpack/webpack-dev-middleware#publicpath
+      */
+      publicPath: '/assets/'
+    }
+    // proxy: {
+    //   "/comments": {
+    //     target: "https://m.weibo.cn",
+    //     changeOrigin: true,
+    //     logLevel: "debug",
+    //     headers: {
+    //       Cookie: ""
+    //     }
+    //   }
+    // },
+    // historyApiFallback: {
+    //   rewrites: [{
+    //     from: /.*/,
+    //     to: "/index.html"
+    //   }]
+    // }
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
   }
-};
+
+}
