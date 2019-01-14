23833307848dd4af97aab643e2c448141e2d92a3
wefashe <wefashe@qq.com>
2019-01-14 19:32:13 +0800

    优化了git-pages分支配置

--- deploy.sh
+++ deploy.sh
@@ -6,20 +6,28 @@ set -e
 # 构建
 npm run build
 
-# 进入生成的构建文件夹
-cd dist
+# 克隆代码
+git clone https://${GH_REF} .deploy_git
+cd .deploy_git
+git checkout gh-pages
+
+cd ../
+\cp -rf dist/* .deploy_git
+cd .deploy_git
 
 # 如果你是要部署到自定义域名
 # echo 'www.example.com' > CNAME
 
-git init
+git config user.name "wenfs"
+git config user.email "wefashe@qq.com"
+
 git add -A
-git commit -m 'deploy :construction:'
+git commit -m ":construction: travis CI Auto deploy at `date +"%Y-%m-%d %H:%M"`"
 
 # 如果你想要部署到 https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果你想要部署到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
-git push -f https://github.com/wefashe/wenfsblog.git master:gh-pages
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages
 
